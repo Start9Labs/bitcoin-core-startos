d7818f5782cf169e62731c6749e72a46179f41d0
Shadowy Super Coder <musashidisciple@proton.me>
2025-10-29 13:39:35 -0600

    remove unused imports

--- startos/install/versions/v30_0_0_1-beta.1.ts
+++ startos/install/versions/v30_0_0_1-beta.1.ts
@@ -1,7 +1,4 @@
-import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
-import { bitcoinConfFile } from '../../fileModels/bitcoin.conf'
-import { bitcoinConfDefaults } from '../../utils'
-
+import { VersionInfo } from '@start9labs/start-sdk'
 export const v30_0_0_1_beta1 = VersionInfo.of({
   version: '30.0.0:1-beta.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
