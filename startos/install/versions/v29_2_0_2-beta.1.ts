35811edd26b883ef9d1d104d3bb816315e1cd48d
Shadowy Super Coder <musashidisciple@proton.me>
2025-12-02 10:18:49 -0700

    bump pre-release tag

--- startos/install/versions/v29_2_0_2-beta.1.ts
+++ startos/install/versions/v29_2_0_2-beta.1.ts
@@ -1,6 +1,6 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
-export const v29_2_0_2beta0 = VersionInfo.of({
-  version: '29.2:2-beta.0',
+export const v29_2_0_2beta1 = VersionInfo.of({
+  version: '29.2:2-beta.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
