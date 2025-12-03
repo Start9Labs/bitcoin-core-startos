b9330c60f1d5db8716e8b207cfff5da2ccec8e21
Gary Krause <gary.krause@mara.com>
2025-12-03 15:40:48 -0500

    refactor: nest IPC config in "Other Settings"

--- startos/actions/config/other.ts
+++ startos/actions/config/other.ts
@@ -4,6 +4,7 @@ import * as diskusage from 'diskusage'
 import { bitcoinConfFile } from '../../fileModels/bitcoin.conf'
 import { bitcoinConfDefaults } from '../../utils'
 import { T } from '@start9labs/start-sdk'
+import { storeJson } from '../../fileModels/store.json'
 
 const {
   coinstatsindex,
@@ -17,6 +18,7 @@ const {
   peerblockfilters,
   peerbloomfilters,
   blocknotify,
+  enableIpc,
 } = bitcoinConfDefaults
 
 const { InputSpec, Value } = sdk
@@ -146,6 +148,14 @@ const configSpec = sdk.InputSpec.of({
     warning:
       'This is ONLY for use with Bisq integration, please use Block Filters for all other applications.',
   }),
+  enableIpc: Value.toggle({
+    name: 'Enable IPC',
+    description:
+      'Enable inter-process communication (IPC) via Unix socket. This allows other services to communicate with Bitcoin Core using a high-performance local socket connection. The socket path will be displayed in Runtime Information.',
+    warning:
+      'IPC is an experimental feature. Only enable this if you know what you are doing with the IPC socket. An example use case would be Stratum v2 mining services.',
+    default: false,
+  }),
 })
 
 export const config = sdk.Action.withInput(
@@ -176,6 +186,8 @@ async function read(effects: any): Promise<PartialConfigSpec> {
   const bitcoinConf = await bitcoinConfFile.read().const(effects)
   if (!bitcoinConf) return {}
 
+  const store = await storeJson.read().const(effects)
+
   return {
     zmqEnabled:
       !!bitcoinConf?.zmqpubhashblock &&
@@ -203,6 +215,7 @@ async function read(effects: any): Promise<PartialConfigSpec> {
       peerblockfilters: bitcoinConf.peerblockfilters,
     },
     peerbloomfilters: bitcoinConf.peerbloomfilters,
+    enableIpc: store?.enableIpc !== undefined ? store.enableIpc : enableIpc,
   }
 }
 
@@ -236,6 +249,21 @@ async function write(effects: T.Effects, input: ConfigSpec) {
   }
 
   await bitcoinConfFile.merge(effects, otherConfig)
+
+  // Check if IPC setting changed (requires restart since it changes the binary)
+  const currentStore = await storeJson.read().const(effects)
+  const currentEnableIpc = currentStore?.enableIpc !== undefined ? currentStore.enableIpc : enableIpc
+  const newEnableIpc = input.enableIpc !== undefined ? input.enableIpc : enableIpc
+
+  // Store enableIpc separately in store.json
+  await storeJson.merge(effects, {
+    enableIpc: newEnableIpc,
+  })
+
+  // Restart if IPC setting changed (switches between bitcoind and bitcoin-node)
+  if (currentEnableIpc !== newEnableIpc) {
+    await sdk.restart(effects)
+  }
 }
 
 type ConfigSpec = typeof configSpec._TYPE
--- startos/actions/index.ts
+++ startos/actions/index.ts
@@ -3,7 +3,6 @@ import { config } from './config/other'
 import { mempoolConfig } from './config/mempool'
 import { peerConfig } from './config/peers'
 import { rpcConfig } from './config/rpc'
-import { ipcConfig } from './config/ipc'
 import { deleteCoinstatsIndex } from './deleteCoinstatsIndex'
 import { deletePeers } from './deletePeers'
 import { deleteRpcAuth } from './deleteRpcAuth'
@@ -30,4 +29,3 @@ export const actions = sdk.Actions.of()
   .addAction(peerConfig)
   .addAction(generateRpcUserDependent)
   .addAction(assumeutxo)
-  .addAction(ipcConfig)
