b30c68c8da135d734ff5fff891c22e197bf8d6be
Matt Hill <mattnine@protonmail.com>
2026-03-24 17:55:45 -0600

    sdk beta.66 + version bump

--- startos/install/versions/index.ts
+++
@@ -1,3 +0,0 @@
-export { v_28_3_5_b4 as current } from './v28.3.5.b4'
-
-export const other = []
--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { current, other } from './versions'
+import { v_28_3_5_b5 } from './v28.3.5.b5'
 
 export const versionGraph = VersionGraph.of({
-  current,
-  other,
+  current: v_28_3_5_b5,
+  other: [],
 })
