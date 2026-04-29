f82b62f2468e35a4c137d19b5c310ede64d0ad8f
Aiden McClelland <me@drbonez.dev>
2026-04-28 23:06:33 -0600

    Bump version to 29.3:9

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,8 +1,9 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v_29_3_8 } from './v29.3.8'
 import { v_28_3_8 } from './v28.3.8'
+import { v_29_3_9 } from './v29.3.9'
 
 export const versionGraph = VersionGraph.of({
-  current: v_29_3_8,
-  other: [v_28_3_8],
+  current: v_29_3_9,
+  other: [v_29_3_8, v_28_3_8],
 })
---
+++ startos/versions/v29.3.9.ts
@@ -0,0 +1,16 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+
+export const v_29_3_9 = VersionInfo.of({
+  version: '29.3:9',
+  releaseNotes: {
+    en_US: 'Internal updates (start-sdk 1.3.3); remove leftover chainstate.old at startup and exclude it from backups',
+    es_ES: 'Actualizaciones internas (start-sdk 1.3.3); eliminar chainstate.old residual al iniciar y excluirlo de las copias de seguridad',
+    de_DE: 'Interne Aktualisierungen (start-sdk 1.3.3); übrig gebliebenes chainstate.old beim Start entfernen und von Backups ausschließen',
+    pl_PL: 'Aktualizacje wewnętrzne (start-sdk 1.3.3); usunięcie pozostałego chainstate.old przy uruchomieniu i wykluczenie go z kopii zapasowych',
+    fr_FR: 'Mises à jour internes (start-sdk 1.3.3) ; suppression du chainstate.old résiduel au démarrage et exclusion des sauvegardes',
+  },
+  migrations: {
+    up: async ({ effects }) => {},
+    down: IMPOSSIBLE,
+  },
+})
